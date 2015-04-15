928f8b595588eab8b142afcffc2b2aabf94720fa
Jacob McCarthy <mccarjac@gmail.com>
2015-04-14 17:52:22 -0700

    Problem2 and 6

---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1;
+            int second = 2;
+            int sum = 0;
+            int temp;
+
+            while (second < 4000000)
+            {
+                temp = second;
+                second = first + second;
+                first = temp;
+                if (first % 2 == 0)
+                {
+                    sum += first;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem6/Problem6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SumSquare = 0;
+            int SquareSum = 0;
+            for(int i = 0;i <= 100;i++)
+            {
+                SumSquare += Convert.ToInt32(Math.Pow(i, 2));
+                SquareSum += i;
+            }
+            SquareSum = Convert.ToInt32(Math.Pow(SquareSum, 2));
+            int Result = SquareSum - SumSquare;
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+    }
+}
