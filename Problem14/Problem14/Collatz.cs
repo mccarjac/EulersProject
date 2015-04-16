85a89b1e8aea5f31b3efb1c78cf8a45fefbd9438
Jacob McCarthy <mccarjac@gmail.com>
2015-04-15 19:24:07 -0700

    problem14

---
+++ Problem14/Problem14/Collatz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem14
+{
+    public class Collatz
+    {
+        long MaxChain;
+        public long MaxNum;
+
+        public Collatz()
+        {
+            MaxChain = 0;
+            MaxNum = 0;
+            Calculate();
+        }
+
+        public void Calculate()
+        {
+            long val;
+            long tempchain;
+            for(long i = 1;i < 1000000;i++)
+            {
+                val = i;
+                tempchain = 1;
+                while(val != 1)
+                {
+                    if (val % 2 == 0)
+                        val = val / 2;
+                    else
+                        val = (3 * val) + 1;
+                    tempchain += 1;
+                }
+
+                if(tempchain > MaxChain)
+                {
+                    MaxChain = tempchain;
+                    MaxNum = i;
+                }
+            }
+        }
+
+    }
+}
---
+++ Problem14/Problem14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collatz sln = new Collatz();
+
+            Console.WriteLine(sln.MaxNum);
+            Console.ReadKey();
+        }
+    }
+}
