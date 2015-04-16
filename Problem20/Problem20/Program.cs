50da39a23e6727c9206715f26a4573bb9a825c11
Jacob McCarthy <mccarjac@gmail.com>
2015-04-15 19:57:04 -0700

    problem20

---
+++ Problem20/Problem20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger val = new BigInteger();
+            val = 1;
+            for(int i = 2;i <= 100;i++)
+            {
+                val = BigInteger.Multiply(val, i);
+            }
+
+            string str = val.ToString();
+
+            val = 0;
+            foreach(char a in str)
+            {
+                val = BigInteger.Add(val, (int)a - 48);
+            }
+
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+    }
+}
