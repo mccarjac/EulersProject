653b629d7b3cde4ba47e791bde62d9b83df8304e
Jacob McCarthy <mccarjac@gmail.com>
2015-04-15 20:48:22 -0700

    problem25

---
+++ Problem25/Problem25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BigInteger first = new BigInteger();
+            BigInteger second = new BigInteger();
+            BigInteger temp = new BigInteger();
+            first = 1;
+            second = 1;
+            long count = 2;
+            while(second.ToString().Length < 1000)
+            {
+                temp = second;
+                second = BigInteger.Add(first, second);
+                first = temp;
+                count += 1;
+            }
+
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
