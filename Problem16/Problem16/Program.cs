c2b191416bf6d0c1d4e697f8d31acc15ea6f8b0b
Jacob McCarthy <mccarjac@gmail.com>
2015-04-15 19:48:03 -0700

    problem16

---
+++ Problem16/Problem16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger num = new BigInteger();
+            num = BigInteger.Pow(2, 1000);
+            string str = num.ToString();
+
+            BigInteger val = new BigInteger();
+            val = 0;
+            foreach(char a in str)
+            {
+                val = BigInteger.Add(val, (int)a - 48);
+            }
+
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+    }
+}
